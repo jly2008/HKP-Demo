fc7be025c84989939674d6690444319f20dba581
ChrisJiang <45065630+jly2008@users.noreply.github.com>
2019-10-22 18:56:59 +0800

    10.22-v1.0

---
+++ files/微信公众号首页/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="微信公众号首页.html",d="generationDate",e=new Date(1571741305253.63),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="052a286d2dd444d5bcdddd29fff5731e",q="type",r="Axure:Page",s="name",t="微信公众号首页",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="88d537a36ccc40b3bd4f9f8da25de3cf",Y="label",Z="friendlyType",ba="图片",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=378,bj="height",bk=689,bl="imageOverrides",bm="images",bn="normal~",bo="images/微信公众号首页/u1734.jpg",bp="masters",bq="objectPaths",br="88d537a36ccc40b3bd4f9f8da25de3cf",bs="scriptId",bt="u1734";
+return _creator();
+})());
